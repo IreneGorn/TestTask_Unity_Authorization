ba624e7ae41c3dfe9627f73c42fe2a5a2d12696d
Irina Gornovskaya <gornowsckaia.irina201372@gmail.com>
2024-01-05 22:00:30 +0300

    Added controller, code refactoring

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,33 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoginController : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField loginInput;
-    [SerializeField] private TMP_InputField passwordInput;
-    [SerializeField] private Button loginButton;
+    [SerializeField] private TMP_InputField _loginInput;
+    [SerializeField] private TMP_InputField _passwordInput;
+    [SerializeField] private Button _loginButton;
 
-    private ServerManager serverManager;
+    private ServerManager _serverManager;
 
     private void Start()
     {
-        loginButton.onClick.AddListener(Login);
+        _loginButton.onClick.AddListener(Login);
     }
 
     private void Awake()
     {
-        serverManager = new ServerManager();
+        _serverManager = new ServerManager();
     }
 
-    public async void Login()
+    private async void Login()
     {
-        var login = loginInput.text;
-        var password = passwordInput.text;
+        var login = _loginInput.text;
+        var password = _passwordInput.text;
 
-        var response = await serverManager.Login(login, password);
+        var response = await _serverManager.Login(login, password);
 
         if (response == null)
         {
--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Cysharp.Threading.Tasks;
 
 public class ServerManager
 {
-    private const string BASE_URL = "https://stage.arenagames.api.ldtc.space/api/v3/gamedev/client/auth/sign-in/";
+    private const string BaseURL = "https://stage.arenagames.api.ldtc.space/api/v3/gamedev/client/auth/sign-in/";
     
         public async UniTask<string> Login(string login, string password)
         {
@@ -14,7 +12,7 @@ public class ServerManager
             form.AddField("login", login);
             form.AddField("password", password);
     
-            using (var www = UnityWebRequest.Post(BASE_URL, form))
+            using (var www = UnityWebRequest.Post(BaseURL, form))
             {
                 try
                 {
